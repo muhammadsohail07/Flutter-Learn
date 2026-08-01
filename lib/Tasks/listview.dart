dab66ee8bb9cd7634e89fde585981f04f3cf3df4
Sohail <anonymousx0766@gmail.com>
2026-08-01 23:33:59 +0500

    list view example update

--- lib/Tasks/listview.dart
+++ lib/Tasks/listview.dart
@@ -5,22 +5,47 @@ class listviewexample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Go to Gym'),
-            subtitle: Text('Go to Gym at 6:00 AM'),
-          ),
-          ListTile(
-            title: Text('Go to College'),
-            subtitle: Text('Go to College at 8:00 AM'),
-          ),
-          ListTile(
-            title: Text('Go to Office'),
-            subtitle: Text('Go to Office at 11:00 AM'),
-          ),
+      appBar: AppBar(title: Text("List view"),),
+      body: Center(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('Go to Gym'),
+              subtitle: Text('Go to Gym at 6:00 AM'),
+            ),
+            ListTile(
+              title: Text('Go to College'),
+              subtitle: Text('Go to College at 8:00 AM'),
+            ),
+            ListTile(
+              title: Text('Go to Office'),
+              subtitle: Text('Go to Office at 11:00 AM'),
+            ),
+            Text("Contacts",textAlign: TextAlign.center , style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.bold
+            )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('John Doe'),
+              subtitle: Text('555-555-5555'),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Jane Doe'),
+              subtitle: Text('555-555-5555'),
+              trailing: Icon(Icons.call),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('John Smith'),
+              subtitle: Text('555-555-5555'),
+              trailing: Icon(Icons.call),
+            ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
