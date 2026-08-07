824d821a4def39e95068ea793463a08334045ca3
Sohail <anonymousx0766@gmail.com>
2026-08-07 23:12:58 +0500

    service file for quote app

--- lib/Tasks/Quote App/quoteapp.dart
+++ lib/Tasks/Quote App/quoteapp.dart
@@ -1,1 +1,11 @@
+import 'dart:convert';
 
+import 'package:flutter_series/Tasks/Quote%20App/modelquoteapp.dart';
+ class ServiceQuoteApp{
+   Future<List<quote>> getQuotes async{
+     final response = await;
+   http.get(Uri.parse("https://api.jsonbin.io/v3/b/6a74c9b3f5f4af5e29f3e532"));
+   final json = jsonDecode(response.body).cast<Map< String, dynamic>>();
+   return json.map<quote>((json)=>quote.fromJson(json)).toList();
+ }
+ }
