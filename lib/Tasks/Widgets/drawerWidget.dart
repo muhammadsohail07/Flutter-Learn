8fda911681ede593745904798ff8fb05f4d6a2db
Sohail <anonymousx0766@gmail.com>
2026-08-09 14:42:32 +0500

    updated (snackbar added in drawer)

--- lib/Tasks/Widgets/drawerWidget.dart
+++ lib/Tasks/Widgets/drawerWidget.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_series/main.dart';
+
 class MyDrawerWidget extends StatelessWidget {
   const MyDrawerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(   // <-- Scaffold nahi, seedha Drawer return karo
+    return Drawer(
+      // <-- Scaffold nahi, seedha Drawer return karo
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
@@ -22,6 +24,7 @@ class MyDrawerWidget extends StatelessWidget {
             subtitle: Text('Go to homepage'),
             trailing: Icon(Icons.arrow_forward_ios),
             onTap: () {
+              Navigator.pop(context);
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => Home()),
@@ -30,8 +33,26 @@ class MyDrawerWidget extends StatelessWidget {
           ),
           ListTile(title: const Text('Categories'), onTap: () {}),
           ListTile(title: const Text('Orders'), onTap: () {}),
-          ListTile(title: const Text('Wishlist'), onTap: () {}),
+          ListTile(
+            leading: Icon(Icons.snapchat),
+            title: const Text('Wishlist'),
+            onTap: () {
+              Navigator.pop(context); // drawer close
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Wishlist screen not ready")),
+              );
+            },
+          ),
           ListTile(title: const Text('Settings'), onTap: () {}),
+          ListTile(
+            
+            title: const Text('Exit'),
+            
+            leading: Icon(Icons.logout),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
         ],
       ),
     );
