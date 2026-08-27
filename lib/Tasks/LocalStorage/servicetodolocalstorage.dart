73c15dda0a804ac7295401e4a3f50439c86f7737
Sohail <anonymousx0766@gmail.com>
2026-08-27 13:26:41 +0500

    save data

--- lib/Tasks/LocalStorage/servicetodolocalstorage.dart
+++ lib/Tasks/LocalStorage/servicetodolocalstorage.dart
@@ -5,12 +5,18 @@ import 'package:flutter_series/Tasks/LocalStorage/modeltodolocalstorage.dart';
 class todoStorageService{
   static const String _storageKey = "todoList";
 
-  Future<List<todo>> loadtodo () async{
+  Future<List<todo>> loadTodo () async{
     final prefs = await SharedPreferences.getInstance();
     final  String? todoString = prefs.getString(_storageKey);
     if (todoString == null) return [];
     final List<dynamic> decoded = jsonDecode(todoString);
     return decoded.map((item) => todo.fromJson(item)).toList();
+  }
+  Future<void> saveTodos(List<todo> todos) async {
+    final prefs = await SharedPreferences.getInstance();
+    final String encoded =
+    jsonEncode(todos.map((todo) => todo.toJson()).toList());
+    await prefs.setString(_storageKey, encoded);
   }
 
 }
