92525cbfe50fd68064a01f23b9bb6aef0fa980cb
Sohail <anonymousx0766@gmail.com>
2026-08-28 22:49:47 +0500

    function added

--- lib/Tasks/LocalStorage/crud.dart
+++ lib/Tasks/LocalStorage/crud.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class crud extends StatefulWidget {
   const crud({super.key});
@@ -8,6 +9,39 @@ class crud extends StatefulWidget {
 }
 
 class _crudState extends State<crud> {
+  final nameController = TextEditingController();
+  String savedname = "no name saved";
+
+  Future<void> savedname() async{
+   final prefs =  await SharedPreferences.getInstance();
+   String name = nameController.text;
+
+   await prefs.setString("username", name);
+   setState(() {
+     savedname = name;
+   });
+   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+     content: Text("name saved successfully"),
+   ));
+  }
+
+  Future<void> getnamed() async{
+    final prefs = await SharedPreferences.getInstance();
+    String? name = prefs.getString("username");
+    setState(() {
+      savedname = name?? "no name saved";
+    });
+  }
+
+  Future<void> deletenamed() async  {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("username");
+    setState(() {
+
+      savedname = "no name save";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
