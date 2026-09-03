f57a3ecf25c5b62853734abe4d03e5cf9169982c
Sohail <anonymousx0766@gmail.com>
2026-09-03 22:39:57 +0500

    login

---
+++ lib/Tasks/API/LOGIN/loginmodel.dart
@@ -0,0 +1,27 @@
+class UserModel {
+  final String email;
+  final String password;
+  final String name;
+
+  UserModel({
+    required this.email,
+    required this.password,
+    required this.name,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      email: json['email'] ?? '',
+      password: json['password'] ?? '',
+      name: json['name'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+      'name': name,
+    };
+  }
+}
