37cea901b8ac5e0387f9e72daf54ec22b4dca34f
Sohail <anonymousx0766@gmail.com>
2026-09-05 22:29:05 +0500

    apis

--- lib/Tasks/API/LOGIN/apiservice.dart
+++ lib/Tasks/API/LOGIN/apiservice.dart
@@ -2,10 +2,13 @@ import 'package:flutter_series/Tasks/API/LOGIN/loginmodel.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 class ApiService {
-  static const String binUrl = 'https://api.jsonbin.io/v3/b/6a9717aada38895dfe2c3390';
+  static const String binUrl = 'https://api.jsonbin.io/v3/b/6a9c4f24f5f4af5e296eca9d';
   static const String apiKey = r'$2a$10$e3kbIVIVSzjC/XvLso.qWeTyzULo/f1TEh3/sxuHo67RElU5l3Gbe';
 
-  Future<UserModel?> login(String email, String password) async {
+
+
+
+   Future<UserModel?> login(String email, String password) async {
     try {
       final response = await http.get(
         Uri.parse(binUrl),
@@ -17,14 +20,15 @@ class ApiService {
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
+        final record = data['record'];
+        final usersList = record['users'] as List<dynamic>;
 
-        // JSONBin response mein actual data "record" ke andar hota hai
-        final user = UserModel.fromJson(data['record']);
-
-        if (user.email == email && user.password == password) {
-          return user;
+        for (var userJson in usersList) {
+          final user = UserModel.fromJson(userJson);
+          if (user.email == email && user.password == password) {
+            return user;
+          }
         }
-
         return null;
       } else {
         throw Exception('API Error: ${response.statusCode}');
