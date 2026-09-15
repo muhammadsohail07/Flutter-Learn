bfccd1e292932861a990e46281922473a68dca3e
Sohail <anonymousx0766@gmail.com>
2026-09-15 22:18:14 +0500

    cloudanary

--- lib/Tasks/API/LOGIN/loginmodel.dart
+++ lib/Tasks/API/LOGIN/loginmodel.dart
@@ -2,11 +2,14 @@ class UserModel {
   final String email;
   final String password;
   final String name;
+  final String? profileImage;
 
   UserModel({
     required this.email,
     required this.password,
     required this.name,
+    required this.profileImage,
+
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
@@ -14,6 +17,7 @@ class UserModel {
       email: json['email'] ?? '',
       password: json['password'] ?? '',
       name: json['name'] ?? '',
+      profileImage: json['profileImage'],
     );
   }
 
@@ -22,6 +26,7 @@ class UserModel {
       'email': email,
       'password': password,
       'name': name,
+      'profileImage' : profileImage,
     };
   }
 }
