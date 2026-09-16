0379f2d80f46ac578506de458d6612cae5aadf8a
Sohail <anonymousx0766@gmail.com>
2026-09-16 17:51:12 +0500

    image upload

---
+++ lib/Tasks/API/LOGIN/cloudinaryservice.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class CloudinaryService {
+  static const String cloudName = '';
+  static const String uploadPreset = '';
+
+  static Future<String> uploadImage(File imageFile) async {
+    final url = Uri.parse(
+      'https://api.cloudinary.com/v1_1/$cloudName/image/upload',
+    );
+
+    final request = http.MultipartRequest('POST', url)
+      ..fields['upload_preset'] = uploadPreset
+      ..files.add(await http.MultipartFile.fromPath('file', imageFile.path));
+
+    final response = await request.send();
+    final responseBody = await response.stream.bytesToString();
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(responseBody);
+      return data['secure_url'] as String;
+    } else {
+      throw Exception('Image upload failed: ${response.statusCode}');
+    }
+  }
+}
