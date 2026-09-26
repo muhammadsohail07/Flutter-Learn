8810a1404d30d418190aea7bcacfc734976c7068
Sohail <anonymousx0766@gmail.com>
2026-09-26 17:27:41 +0500

    update

--- lib/Tasks/API/LOGIN/homescreen.dart
+++ lib/Tasks/API/LOGIN/homescreen.dart
@@ -28,11 +28,11 @@ class _HomeScreenState extends State<HomeScreen> {
     final hour = DateTime.now().hour;
 
     if (hour < 12) {
-      return 'Good Morning';
+      return 'Good Morning!';
     } else if (hour < 17) {
-      return 'Good Afternoon';
+      return 'Good Afternoon!';
     } else {
-      return 'Good Evening';
+      return 'Good Evening!';
     }
   }
 
